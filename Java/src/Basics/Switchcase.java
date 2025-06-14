88647be0459759e35cb0cbf3bb594dcf7da8dced
Parimala1321 <marriparimala44@gmail.com>
2025-06-14 12:20:21 +0530

    modified switchcase

--- Java/src/Basics/Switchcase.java
+++ Java/src/Basics/Switchcase.java
@@ -31,6 +31,51 @@ public class Switchcase {
 		 default :
 			 System.out.println("Enter the valid number");
 		}
+		
+		
+		//seasons example
+		 String Month = "January";
+		  switch(Month) {
+		  case "December","January","Febuary":
+			  System.out.println("Winter");
+		      break;
+		  case "March","April","May":
+			  System.out.println("Summer");
+		      break;
+		  case "June","July","August" :
+			  System.out.println("Rainy");
+		      break;
+		      default :
+		    	  System.out.println("Spring");
+		    	  
+		  }
+		  
+		  
+		  
+		  
+		  // updated switch statement
+		  String Months = "November";
+		  switch(Months) {
+		  case "December","January","Febuary" -> System.out.println("Winter");
+		  case "March","April","May" ->  System.out.println("Summer"); 
+		  case "June","July","August" -> System.out.println("Rainy");
+	      default  ->  System.out.println("Spring");
+		    	  
+		  }
+		  
+		  
+		  
+		  //assigning the result
+		  String Mon = "April";
+		  String Result = "";
+		  Result =  switch(Mon) {
+		       case "December","January","Febuary" -> "Winter";
+		       case "March","April","May" ->  "Summer"; 
+		       case "June","July","August" -> "Rainy";
+	           default  ->  "Spring";
+		    	  
+		  };
+		  System.out.println(Result);
 	}
 
 }
