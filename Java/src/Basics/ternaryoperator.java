320dce993811701a5423b477474db673812b1849
Parimala1321 <marriparimala44@gmail.com>
2025-06-14 11:16:24 +0530

    I am Pushing Ternaryoperator program

---
+++ Java/src/Basics/ternaryoperator.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class ternaryoperator {
+
+	public static void main(String[] args) {
+	
+		int num = 5;
+		int result = 0;
+		result = num % 2== 0 ? 10: 20;
+		System.out.println(result);
+
+	}
+
+}
